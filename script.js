5581883f2fb9875f8b074b58363740e83f604fe7
Jinji <127928909+ProstoGio@users.noreply.github.com>
2026-01-07 22:59:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -4,44 +4,44 @@ button.addEventListener("click", function (event) {
   let ind = true;
   if (document.getElementById("firstnameinput").value == "") {
     ind = false;
-    document.getElementById("firstnameerror").style.color =
-      "rgba(255, 0, 0, 1)";
-    document.getElementById("firstnameinput").style.backgroundColor =
-      "rgba(255, 0, 0, 0.1)";
+    document.getElementById("firstnameerror").style.visibility =
+      "visible";
+    document.getElementById("firstnameinput").style.borderColor =
+      "rgb(255,0,0)";
   } else {
-    document.getElementById("firstnameerror").style.color =
-      "rgba(255, 0, 0, 0)";
-    document.getElementById("firstnameinput").style.backgroundColor =
-      "rgba(255, 0, 0, 0)";
+    document.getElementById("firstnameerror").style.visibility =
+      "hidden";
+    document.getElementById("firstnameinput").style.borderColor =
+      "#6386c2";
   }
 
   if (document.getElementById("lastnameinput").value == "") {
     ind = false;
-    document.getElementById("lastnameerror").style.color = "rgba(255, 0, 0, 1)";
-    document.getElementById("lastnameinput").style.backgroundColor =
-      "rgba(255, 0, 0, 0.1)";
+    document.getElementById("lastnameerror").style.visibility = "visible";
+    document.getElementById("lastnameinput").style.borderColor =
+      "rgb(255, 0, 0)";
   } else {
-    document.getElementById("lastnameerror").style.color = "rgba(255, 0, 0, 0)";
-    document.getElementById("lastnameinput").style.backgroundColor =
-      "rgba(255, 0, 0, 0)";
+    document.getElementById("lastnameerror").style.visibility = "hidden";
+    document.getElementById("lastnameinput").style.borderColor =
+      "#6386c2";
   }
 
   if (document.getElementById("emailinput").checkValidity()) {
-    document.getElementById("emailerror").style.color = "rgba(255, 0, 0, 0)";
-    document.getElementById("emailinput").style.backgroundColor =
-      "rgba(255, 0, 0, 0)";
+    document.getElementById("emailerror").style.visibility = "hidden";
+    document.getElementById("emailinput").style.borderColor =
+      "#6386c2";
   } else {
     ind = false;
     if (document.getElementById("emailinput").value == "") {
       document.getElementById("emailerror").innerHTML = "Enter Email!";
-      document.getElementById("emailerror").style.color = "rgba(255, 0, 0, 1)";
-      document.getElementById("emailinput").style.backgroundColor =
-        "rgba(255, 0, 0, 0.1)";
+      document.getElementById("emailerror").style.visibility = "visible";
+      document.getElementById("emailinput").style.borderColor =
+        "rgb(255,0,0)";
     } else {
       document.getElementById("emailerror").innerHTML = "Invalid Email!";
-      document.getElementById("emailerror").style.color = "rgba(255, 0, 0, 1)";
-      document.getElementById("emailinput").style.backgroundColor =
-        "rgba(255, 0, 0, 0.1)";
+      document.getElementById("emailerror").style.visibility = "visible";
+      document.getElementById("emailinput").style.borderColor =
+        "rgb(255,0,0)";
     }
   }
 
@@ -49,33 +49,33 @@ button.addEventListener("click", function (event) {
   if (passsize.length < 8) {
     if (passsize.length == 0) {
       document.getElementById("passworderror").innerHTML = "Enter Password!";
-      document.getElementById("passworderror").style.color =
-        "rgba(255, 0, 0, 1)";
-      document.getElementById("passwordinpbox").style.backgroundColor =
-        "rgba(255, 0, 0, 0.1)";
+      document.getElementById("passworderror").style.visibility =
+        "visible";
+      document.getElementById("passwordinpbox").style.borderColor =
+        "rgb(255, 0, 0)";
     } else {
       document.getElementById("passworderror").innerHTML =
         "Length must be more than 8 characters!";
-      document.getElementById("passworderror").style.color =
-        "rgba(255, 0, 0, 1)";
-      document.getElementById("passwordinpbox").style.backgroundColor =
-        "rgba(255, 0, 0, 0.1)";
+      document.getElementById("passworderror").style.visibility =
+        "visible";
+      document.getElementById("passwordinpbox").style.borderColor =
+        "rgb(255, 0, 0)";
     }
   } else {
-    document.getElementById("passworderror").style.color = "rgba(255, 0, 0, 0)";
+    document.getElementById("passworderror").style.visibility = "hidden";
     document.getElementById("passwordinpbox").style.backgroundColor =
-      "rgba(255, 0, 0, 0)";
+      "#6386c2";
   }
 
   if (document.getElementById("countryselector").value == "") {
     ind = false;
-    document.getElementById("countryerror").style.color = "rgba(255, 0, 0, 1)";
-    document.getElementById("countryselector").style.backgroundColor =
-      "rgba(255, 0, 0, 0.1)";
+    document.getElementById("countryerror").style.visibility = "visible";
+    document.getElementById("countryselector").style.borderColor =
+      "rgb(255, 0, 0)";
   } else {
-    document.getElementById("countryerror").style.color = "rgba(255, 0, 0, 0)";
-    document.getElementById("countryselector").style.backgroundColor =
-      "rgba(255, 0, 0, 0)";
+    document.getElementById("countryerror").style.visibility = "hidden";
+    document.getElementById("countryselector").style.borderColor =
+      "#6386c2";
   }
 
   const c1 = document.getElementById("js");
@@ -98,9 +98,9 @@ button.addEventListener("click", function (event) {
     c8.checked == false
   ) {
     ind = false;
-    document.getElementById("checkboxerror").style.color = "rgba(255, 0, 0, 1)";
+    document.getElementById("checkboxerror").style.visibility = "visible";
   } else {
-    document.getElementById("checkboxerror").style.color = "rgba(255, 0, 0, 0)";
+    document.getElementById("checkboxerror").style.visibility = "hidden";
   }
 
   if (ind == true) window.location.href = "page2.html";
